d3354686b32a6b888661d7cd69da888f17a91364
hotfireeagle <hotfireeagle@163.com>
2022-08-06 12:10:23 +0800

    refactor

--- nfa.go
+++ nfa.go
@@ -1,125 +1,125 @@
-package main
+// package main
 
-type transition struct {
-	id    int
-	from  int  // from state
-	to    int  // to state
-	input byte // input symbol
-}
+// type transition struct {
+// 	id    int
+// 	from  int  // from state
+// 	to    int  // to state
+// 	input byte // input symbol
+// }
 
-func new_transition(id int, fromStateId int, toStateId int, inputC byte) *transition {
-	return &transition{
-		id:    id,
-		from:  fromStateId,
-		to:    toStateId,
-		input: inputC,
-	}
-}
+// func new_transition(id int, fromStateId int, toStateId int, inputC byte) *transition {
+// 	return &transition{
+// 		id:    id,
+// 		from:  fromStateId,
+// 		to:    toStateId,
+// 		input: inputC,
+// 	}
+// }
 
-type state struct {
-	id          int
-	transitions []*transition
-}
+// type state struct {
+// 	id          int
+// 	transitions []*transition
+// }
 
-func new_state(id int, transitions []*transition) *state {
-	return &state{
-		id:          id,
-		transitions: transitions,
-	}
-}
+// func new_state(id int, transitions []*transition) *state {
+// 	return &state{
+// 		id:          id,
+// 		transitions: transitions,
+// 	}
+// }
 
-// move
-type move struct {
-	start *state
-	end   *state
-}
+// // move
+// type move struct {
+// 	start *state
+// 	end   *state
+// }
 
-func new_move(s, e *state) *move {
-	return &move{
-		start: s,
-		end:   e,
-	}
-}
+// func new_move(s, e *state) *move {
+// 	return &move{
+// 		start: s,
+// 		end:   e,
+// 	}
+// }
 
-type nfa struct {
-	start          int
-	end            int
-	state_map      map[int]*state
-	transition_map map[int]*transition
-}
+// type nfa struct {
+// 	start          int
+// 	end            int
+// 	state_map      map[int]*state
+// 	transition_map map[int]*transition
+// }
 
-func new_nfa() *nfa {
-	return &nfa{
-		start:          -1,
-		end:            -1,
-		state_map:      make(map[int]*state),
-		transition_map: make(map[int]*transition),
-	}
-}
+// func new_nfa() *nfa {
+// 	return &nfa{
+// 		start:          -1,
+// 		end:            -1,
+// 		state_map:      make(map[int]*state),
+// 		transition_map: make(map[int]*transition),
+// 	}
+// }
 
-func (n *nfa) createFromRegexp(str string) {
-	if str == "" {
-		panic("empty regexp")
-	}
+// func (n *nfa) createFromRegexp(str string) {
+// 	if str == "" {
+// 		panic("empty regexp")
+// 	}
 
-	postfix := re2postfix(str)
-	n.createFromPostfix(postfix)
-}
+// 	postfix := re2postfix(str)
+// 	n.createFromPostfix(postfix)
+// }
 
-func (n *nfa) createFromPostfix(postfix string) {
-	if postfix == "" {
-		panic("empty postfix")
-	}
+// func (n *nfa) createFromPostfix(postfix string) {
+// 	if postfix == "" {
+// 		panic("empty postfix")
+// 	}
 
-	uuid_state := 0
-	uuid_transition := 0
+// 	uuid_state := 0
+// 	uuid_transition := 0
 
-	var newState = func() *state {
-		empty_transitions := make([]*transition, 0)
-		uuid_state += 1
-		state := new_state(uuid_state, empty_transitions)
-		n.state_map[uuid_state] = state
-		return state
-	}
+// 	var newState = func() *state {
+// 		empty_transitions := make([]*transition, 0)
+// 		uuid_state += 1
+// 		state := new_state(uuid_state, empty_transitions)
+// 		n.state_map[uuid_state] = state
+// 		return state
+// 	}
 
-	var newTransition = func(fromState *state, toState *state, inputC byte) *transition {
-		uuid_transition += 1
-		transition := new_transition(uuid_transition, fromState.id, toState.id, inputC)
-		fromState.transitions = append(fromState.transitions, transition)
-		n.transition_map[uuid_transition] = transition
-		return transition
-	}
+// 	var newTransition = func(fromState *state, toState *state, inputC byte) *transition {
+// 		uuid_transition += 1
+// 		transition := new_transition(uuid_transition, fromState.id, toState.id, inputC)
+// 		fromState.transitions = append(fromState.transitions, transition)
+// 		n.transition_map[uuid_transition] = transition
+// 		return transition
+// 	}
 
-	moves := new_movestack()
+// 	moves := new_movestack()
 
-	for i := 0; i < len(postfix); i++ {
-		ch := postfix[i]
+// 	for i := 0; i < len(postfix); i++ {
+// 		ch := postfix[i]
 
-		if ch == '|' {
+// 		if ch == '|' {
 
-		} else if ch == '.' {
-			leftVal := moves.out()
-			rightVal := moves.out()
+// 		} else if ch == '.' {
+// 			leftVal := moves.out()
+// 			rightVal := moves.out()
 
-			for _, transition := range rightVal.start.transitions {
-				transition_id := transition.id
-				transition := n.transition_map[transition_id]
-				transition.from = leftVal.end.id
-				leftVal.end.transitions = append(leftVal.end.transitions, transition)
-			}
-		} else if ch == '*' {
+// 			for _, transition := range rightVal.start.transitions {
+// 				transition_id := transition.id
+// 				transition := n.transition_map[transition_id]
+// 				transition.from = leftVal.end.id
+// 				leftVal.end.transitions = append(leftVal.end.transitions, transition)
+// 			}
+// 		} else if ch == '*' {
 
-		} else if ch == '?' {
+// 		} else if ch == '?' {
 
-		} else if ch == '+' {
+// 		} else if ch == '+' {
 
-		} else {
-			// normal character
-			start := newState()
-			end := newState()
-			newTransition(start, end, ch)
-			move := new_move(start, end)
-			moves.in(move)
-		}
-	}
-}
+// 		} else {
+// 			// normal character
+// 			start := newState()
+// 			end := newState()
+// 			newTransition(start, end, ch)
+// 			move := new_move(start, end)
+// 			moves.in(move)
+// 		}
+// 	}
+// }
--- utils.go
+++ utils.go
@@ -1,49 +1,49 @@
-package main
+// package main
 
-type bytestack struct {
-	vals []byte
-}
+// type bytestack struct {
+// 	vals []byte
+// }
 
-func new_bytestack() *bytestack {
-	return &bytestack{
-		vals: make([]byte, 0),
-	}
-}
+// func new_bytestack() *bytestack {
+// 	return &bytestack{
+// 		vals: make([]byte, 0),
+// 	}
+// }
 
-func (s *bytestack) in(val byte) {
-	s.vals = append(s.vals, val)
-}
+// func (s *bytestack) in(val byte) {
+// 	s.vals = append(s.vals, val)
+// }
 
-func (s *bytestack) out() byte {
-	val := s.vals[len(s.vals)-1]
-	s.vals = s.vals[:len(s.vals)-1]
-	return val
-}
+// func (s *bytestack) out() byte {
+// 	val := s.vals[len(s.vals)-1]
+// 	s.vals = s.vals[:len(s.vals)-1]
+// 	return val
+// }
 
-func (s *bytestack) peek() byte {
-	return s.vals[len(s.vals)-1]
-}
+// func (s *bytestack) peek() byte {
+// 	return s.vals[len(s.vals)-1]
+// }
 
-func (s *bytestack) is_empty() bool {
-	return len(s.vals) == 0
-}
+// func (s *bytestack) is_empty() bool {
+// 	return len(s.vals) == 0
+// }
 
-type movestack struct {
-	vals []*move
-}
+// type movestack struct {
+// 	vals []*move
+// }
 
-func new_movestack() *movestack {
-	return &movestack{
-		vals: make([]*move, 0),
-	}
-}
+// func new_movestack() *movestack {
+// 	return &movestack{
+// 		vals: make([]*move, 0),
+// 	}
+// }
 
-func (s *movestack) in(val *move) {
-	s.vals = append(s.vals, val)
-}
+// func (s *movestack) in(val *move) {
+// 	s.vals = append(s.vals, val)
+// }
 
-func (s *movestack) out() *move {
-	val := s.vals[len(s.vals)-1]
-	s.vals = s.vals[:len(s.vals)-1]
-	return val
-}
+// func (s *movestack) out() *move {
+// 	val := s.vals[len(s.vals)-1]
+// 	s.vals = s.vals[:len(s.vals)-1]
+// 	return val
+// }
