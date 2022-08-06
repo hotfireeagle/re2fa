1b612b9d6504b7944829aff7bcd2acdad4b7fe6c
hotfireeagle <hotfireeagle@163.com>
2022-08-06 10:57:17 +0800

    feat: cjam

--- utils.go
+++ utils.go
@@ -27,3 +27,23 @@ func (s *bytestack) peek() byte {
 func (s *bytestack) is_empty() bool {
 	return len(s.vals) == 0
 }
+
+type movestack struct {
+	vals []*move
+}
+
+func new_movestack() *movestack {
+	return &movestack{
+		vals: make([]*move, 0),
+	}
+}
+
+func (s *movestack) in(val *move) {
+	s.vals = append(s.vals, val)
+}
+
+func (s *movestack) out() *move {
+	val := s.vals[len(s.vals)-1]
+	s.vals = s.vals[:len(s.vals)-1]
+	return val
+}
